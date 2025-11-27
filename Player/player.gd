cc3b07b7ae8510bab21f2d25ce3e5497bf4e4905
NathanGRSDesktop <nathangeers00@gmail.com>
2025-11-27 18:13:39 +0100

    feat: Sprinting and main menu functionality. Also animations for walking and sprinting

--- Player/player.gd
+++ Player/player.gd
@@ -1,13 +1,21 @@
 extends CharacterBody3D
 
 # How fast the player moves in meters per second.
-@export var speed = 5.0
+@export var walk_speed = 3.0
+@export var sprint_speed = 4.0
 # How fast the camera moves.
 @export var mouse_sensitivity = 0.1
 
 # Rotation to change the character rotation.
 var rotation_x := 0.0
 var rotation_y := 0.0
+
+var anim_player : AnimationPlayer
+
+func _ready():
+	# Hide the OS cursor and capture it for mouse movement
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	anim_player = get_node("Pivot/Character/AnimationPlayer")
 
 # Makes it so the camera does not rotate when interacting.
 func _unhandled_input(event):
@@ -45,6 +53,13 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		# Move the character by 1 on the x axis. 
 		direction += transform.basis.x
+	
+	var current_speed = walk_speed
+	if Input.is_action_pressed("run"):
+		current_speed = sprint_speed
+		
+	if Input.is_action_pressed("quit"):
+		get_tree().quit()
 
 	# Check if player is moving: 
 	if direction != Vector3.ZERO: 
@@ -52,14 +67,26 @@ func _physics_process(delta):
 		# a speed of 1, it moves at a speed of 1.4, this is why we need to 
 		# normalize the movement.
 		direction = direction.normalized()
+		
+		# Check if the player is sprinting:
+		if Input.is_action_pressed("run"):
+			current_speed = sprint_speed
+			# Change to the running animation.
+			if anim_player.current_animation != "CharacterArmature|Run":
+				anim_player.play("CharacterArmature|Run")
+		else:
+			current_speed = walk_speed
+			# If not sprinting, he is walking.
+			if anim_player.current_animation != "CharacterArmature|Walk":
+				anim_player.play("CharacterArmature|Walk")
+	else:
+		# If not moving, he is idle.
+		if anim_player.current_animation != "CharacterArmature|Idle":
+			anim_player.play("CharacterArmature|Idle")
 
 	# Apply movement
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
+	velocity.x = direction.x * current_speed
+	velocity.z = direction.z * current_speed
 
 	# Move the character
 	move_and_slide()
-
-func _ready():
-	# Hide the OS cursor and capture it for mouse movement
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
